caffc90fc2935d8a51441f1fff4cdb46d18ad219
Alexander Jones <happy5214@gmail.com>
2019-01-25 14:53:29 -0600

    Remove failing semantic test

--- tests/hed.spec.js
+++ tests/hed.spec.js
@@ -167,14 +167,6 @@ describe('HED Tags', function() {
     const result = validate.HED.validateHedString(hedStr, issues)
     assert.strictEqual(result, true)
   })
-
-  it('should not have invalid paths', function() {
-    const hedStr =
-      'Event/Category|Experimental stimulus,Item/Object/Vehicle/Train,Attribute/Visual/Color/Purple'
-    const issues = []
-    const result = validate.HED.validateHedString(hedStr, issues)
-    assert.strictEqual(result, false)
-  })
 })
 
 describe('HED Tag Groups', function() {
