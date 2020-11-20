3f9275c838e0d57cf9794d5b57a25063ead9d536
Alexander Jones <happy5214@gmail.com>
2020-11-20 03:43:55 -0600

    Add function documentation

--- validator/dataset.js
+++ validator/dataset.js
@@ -1,30 +1,61 @@
 const { validateHedString } = require('./hedString')
 
+/**
+ * Parse the dataset's definitions and evaluate labels in the dataset.
+ *
+ * @param {string[]} hedStrings The dataset's HED strings.
+ * @param {Schema} hedSchema The HED schema.
+ * @return {[Definitions, string[]]} The definitions and the evaluated HED strings.
+ */
 const parseDefinitions = function(hedStrings, hedSchema) {
   // TODO: Implement
   return [{}, hedStrings]
 }
 
+/**
+ * Perform dataset-level validation on a HED dataset.
+ *
+ * @param {Definitions} definitions The parsed dataset definitions.
+ * @param {string[]} hedStrings The dataset's HED strings.
+ * @param {Schema} hedSchema The HED schema.
+ * @return {[boolean, object[]]} Whether the HED dataset is valid and any issues found.
+ */
 const validateDataset = function(definitions, hedStrings, hedSchema) {
   // TODO: Implement
   return [true, []]
 }
 
+/**
+ * Validate a group of HED strings.
+ *
+ * @param {string[]} hedStrings A group of HED strings.
+ * @param {Schema} hedSchema The HED schema.
+ * @param {boolean} checkForWarnings Whether to check for warnings or only errors.
+ * @return {Array} Whether the HED strings is valid and any issues found.
+ */
 const validateHedStrings = function(hedStrings, hedSchema, checkForWarnings) {
-  let datasetValid = true
-  let datasetIssues = []
+  let stringsValid = true
+  let stringIssues = []
   for (const hedString of hedStrings) {
     const [valid, issues] = validateHedString(
       hedString,
       hedSchema,
       checkForWarnings,
     )
-    datasetValid = datasetValid && valid
-    datasetIssues = datasetIssues.concat(issues)
+    stringsValid = stringsValid && valid
+    stringIssues = stringIssues.concat(issues)
   }
-  return [datasetValid, datasetIssues]
+  return [stringsValid, stringIssues]
 }
 
+/**
+ * Validate a HED dataset.
+ *
+ * @param {string[]} hedStrings The dataset's HED strings.
+ * @param {Schema} hedSchema The HED schema.
+ * @param {boolean} checkForWarnings Whether to check for warnings or only errors.
+ * @return {Array} Whether the HED dataset is valid and any issues found.
+ */
 const validateHedDataset = function(
   hedStrings,
   hedSchema,
