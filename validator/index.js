dfbaf08ce2fab11355ed184f2309d9ea988e38cd
Alexander Jones <happy5214@gmail.com>
2020-12-21 03:58:12 -0600

    Export new top-level functions publicly

--- validator/index.js
+++ validator/index.js
@@ -1,7 +1,10 @@
+const dataset = require('./dataset')
 const event = require('./event')
 const schema = require('./schema')
 
 module.exports = {
   buildSchema: schema.buildSchema,
+  validateHedDataset: dataset.validateHedDataset,
+  validateHedEvent: event.validateHedEvent,
   validateHedString: event.validateHedString,
 }
