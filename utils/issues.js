00748e218b0c8c6695e5ddcb6a5de26994801632
Alexander Jones <happy5214@gmail.com>
2021-07-29 05:18:45 -0500

    Rescope placeholder parameter and add new issue
    This commit rescopes the allowPlaceholders argument to validateHedString
    to instead signal a BIDS value string, which expects a single value
    placeholder. An error is now reported when this parameter is true but
    no standalone (i.e. non-definition) placeholders are found. The tests
    have been adjusted.

--- utils/issues.js
+++ utils/issues.js
@@ -134,6 +134,11 @@ const generateIssue = function (internalCode, parameters) {
       level = 'error'
       message = `Invalid placeholder in definition - "${parameters.definition}"`
       break
+    case 'missingPlaceholder':
+      hedCode = 'HED_PLACEHOLDER_INVALID'
+      level = 'error'
+      message = `HED value string "${parameters.string}" is missing a required placeholder.`
+      break
     case 'invalidValue':
       hedCode = 'HED_VALUE_INVALID'
       level = 'error'
