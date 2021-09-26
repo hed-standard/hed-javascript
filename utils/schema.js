76fbe635d849ac64230ea987a49f4d9fea6148f2
Alexander Jones <happy5214@gmail.com>
2021-09-26 03:38:37 -0500

    Split setParent into two functions to simplify recursion

--- utils/schema.js
+++ utils/schema.js
@@ -84,18 +84,30 @@ const loadLocalSchema = function (path) {
  * @param {object} node The child node.
  * @param {object} parent The parent node.
  */
-const setParent = function (node, parent) {
+const setNodeParent = function (node, parent) {
   // Assume that we've already run this function if so.
   if ('$parent' in node) {
     return
   }
   node.$parent = parent
-  if (node.node) {
-    for (const child of node.node) {
-      setParent(child, node)
-    }
-  } else if (node.schema) {
-    setParent(node.schema[0], null)
+  const childNodes = node.node || []
+  for (const child of childNodes) {
+    setNodeParent(child, node)
+  }
+}
+
+/**
+ * Handle top level of parent-setting recursion before passing to setNodeParent.
+ *
+ * @param {object} node The child node.
+ * @param {object} parent The parent node.
+ */
+const setParent = function (node, parent) {
+  if (node.schema) {
+    node.$parent = null
+    setNodeParent(node.schema[0], null)
+  } else {
+    setNodeParent(node, parent)
   }
 }
 
