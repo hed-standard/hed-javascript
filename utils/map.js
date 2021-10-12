ba2f508bd2c79ad40c35e45f15198549174e4fdb
Alexander Jones <happy5214@gmail.com>
2021-10-12 11:45:39 -0500

    Create function to filter non-equal duplicates from map construction

---
+++ utils/map.js
@@ -0,0 +1,25 @@
+const isEqual = require('lodash/isEqual')
+
+/**
+ * Filter non-equal duplicates from a key-value list,
+ *
+ * @param {Array} list A list of key-value pairs.
+ * @param {function(*, *): boolean} equalityFunction An equality function for the value data.
+ * @return {[Map<any, any>, Array]} A map and any non-equal duplicate keys found.
+ */
+const filterNonEqualDuplicates = function (list, equalityFunction = isEqual) {
+  const map = new Map()
+  const duplicates = []
+  for (const [key, value] of list) {
+    if (!map.has(key)) {
+      map.set(key, value)
+    } else if (!equalityFunction(map.get(key), value)) {
+      duplicates.push([key, value])
+    }
+  }
+  return [map, duplicates]
+}
+
+module.exports = {
+  filterNonEqualDuplicates: filterNonEqualDuplicates,
+}
