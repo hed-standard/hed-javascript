f271963000d8a807f594ae5719d4c14b0524cbd5
Alexander Jones <happy5214@gmail.com>
2022-01-28 08:17:33 -0600

    Fix test by deleting obsolete function call

--- tests/stringParser.spec.js
+++ tests/stringParser.spec.js
@@ -2,13 +2,12 @@ const assert = require('chai').assert
 const { Schemas } = require('../utils/schema')
 const { buildSchema } = require('../converter/schema')
 const {
-  ParsedHedTag,
-  formatHedTag,
   hedStringIsAGroup,
   parseHedString,
   removeGroupParentheses,
   splitHedString,
 } = require('../validator/stringParser')
+const { ParsedHedTag } = require('../validator/types')
 const generateIssue = require('../utils/issues/issues').generateIssue
 
 describe('HED string parsing', () => {
@@ -368,7 +367,6 @@ describe('HED string parsing', () => {
       }
       validatorWithoutIssues(testStrings, expectedResults, (string) => {
         const parsedTag = new ParsedHedTag(string, string, [], nullSchema)
-        formatHedTag(parsedTag)
         return parsedTag.formattedTag
       })
     })
