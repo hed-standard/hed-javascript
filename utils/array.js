46004e3d23a4c4f623e6d0b776c9ae2276513ecf
Alexander Jones <happy5214@gmail.com>
2022-05-13 01:30:23 -0500

    Add recursive map utility function

--- utils/array.js
+++ utils/array.js
@@ -42,8 +42,25 @@ const asArray = function (array) {
   return Array.isArray(array) ? array : [array]
 }
 
+/**
+ * Apply a function recursively to an array.
+ *
+ * @template T,U
+ * @param {function(T): U} fn The function to apply.
+ * @param {T[]} array The array to map.
+ * @returns {U[]} The mapped array.
+ */
+function recursiveMap(fn, array) {
+  if (Array.isArray(array)) {
+    return array.map((element) => recursiveMap(fn, element))
+  } else {
+    return fn(array)
+  }
+}
+
 module.exports = {
   getElementCount: getElementCount,
   flattenDeep: flattenDeep,
   asArray: asArray,
+  recursiveMap: recursiveMap,
 }
