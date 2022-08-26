ee1028e1b929974d55deb2f61b46ac4408733d24
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2022-08-26 14:42:13 -0500

    Experimented with esbuild

--- common/schema/config.js
+++ common/schema/config.js
@@ -1,14 +1,27 @@
 /** Path to the fallback HED schema. */
+//
+// const path = require('path')
+//
+// // TODO: Delete in 4.0.0.
+// const fallbackFilePath = 'data/HED8.0.0.xml'
+// const fallbackDirectory = path.resolve('..', '..', '..', 'data')
+// const localSchemaList = new Set(['HED8.0.0', 'HED_testlib_1.0.2'])
+//
+// module.exports = {
+//   fallbackFilePath,
+//   fallbackDirectory,
+//   localSchemaList,
+// }
 
-const path = require('path')
+const localSchemaList = new Map([
+  ['HED8.0.0', require('../../data/HED8.0.0.xml')],
+  ['HED_testlib_1.0.2', require('../../data/HED_testlib_1.0.2.xml')],
+])
 
 // TODO: Delete in 4.0.0.
 const fallbackFilePath = 'data/HED8.0.0.xml'
-const fallbackDirectory = path.resolve('data')
-const localSchemaList = new Set(['HED8.0.0', 'HED_testlib_1.0.2'])
 
 module.exports = {
   fallbackFilePath,
-  fallbackDirectory,
   localSchemaList,
 }
