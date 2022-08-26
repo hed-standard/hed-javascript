a263b5f4312bb730f3250e525d8db4703db0a966
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2022-08-26 13:32:53 -0500

    Updated path

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path')
 //const modulePath = require.resolve(path.resolve(require.resolve('.'))
 //const modulePath = 3 //require.resolve(path.resolve('.'))
 
-const modulePath = require.resolve.paths('common')
-
+//const modulePath = require.resolve.paths('common')
+const fallbackDirectoryBase = path.resolve('data')
 module.exports = {
-  modulePath,
+  fallbackDirectoryBase,
   converter,
   validator,
 }
