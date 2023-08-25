82c60410fa6ecd094b2e63864559584770f8cb8c
Alexander Jones <happy5214@gmail.com>
2023-08-25 08:10:18 -0500

    Move XML file transformer to root

--- fileTransformer.js
+++ fileTransformer.js
@@ -1,6 +1,6 @@
 export default {
   process(sourceText, sourcePath, options) {
-    sourceText = sourceText.replace(/\"/g, '\\"').replace(/\n/g, '\\n')
+    sourceText = sourceText.replace(/"/g, '\\"').replace(/\n/g, '\\n')
     return {
       code: `module.exports = "${sourceText}";`,
     }
