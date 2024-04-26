63681e0c6910463b91434b564b1a74506750b539
Alexander Jones <happy5214@gmail.com>
2024-04-26 11:20:04 -0500

    Fix bug in Fetch API implementation

--- utils/files.js
+++ utils/files.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-import fetch, { Request } from 'cross-fetch'
+import fetch from 'cross-fetch'
 
 /**
  * Read a local file.
@@ -23,8 +23,7 @@ export const readFile = function (fileName) {
  * @returns {Promise<string>} A promise with the file contents.
  */
 export const readHTTPSFile = function (url) {
-  const myRequest = new Request(url)
-  return fetch(myRequest).then((response) => {
+  return fetch(url).then((response) => {
     if (!response.ok) {
       throw new Error(`Server responded to ${url} with status code ${response.status}: ${response.statusText}`)
     }
