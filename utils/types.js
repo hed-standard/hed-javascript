cce1cb015791902c126b4bb363ce51438acfa8c5
Alexander Jones <happy5214@gmail.com>
2024-10-04 08:15:28 -0500

    Convert Memoizer back to plain class
    Also declare property map as explicit field.
    
    Fixes #198

--- utils/types.js
+++ utils/types.js
@@ -1,39 +1,46 @@
 /** Utility classes. **/
 
-/**
- * Mix-in/superclass for property memoization until we can get away with private fields.
- */
-export const MemoizerMixin = (Base) => {
-  return class extends Base {
-    constructor(...args) {
-      super(...args)
-      this._memoizedProperties = new Map()
-    }
+import { generateIssue, IssueError } from '../common/issues/issues'
 
-    /**
-     * Memoize the property.
-     *
-     * @template T
-     * @param {string} propertyName The property name
-     * @param {function() : T} valueComputer A function to compute the value.
-     * @returns {T} The computed value.
-     * @protected
-     */
-    _memoize(propertyName, valueComputer) {
-      if (!propertyName) {
-        throw new Error('Invalid property name in Memoizer subclass.')
-      }
-      if (this._memoizedProperties.has(propertyName)) {
-        return this._memoizedProperties.get(propertyName)
-      }
-      const value = valueComputer()
-      this._memoizedProperties.set(propertyName, value)
-      return value
+/**
+ * Superclass for property memoization until we can get away with private fields.
+ */
+export class Memoizer {
+  /**
+   * Map containing memoized properties.
+   *
+   * @type {Map<string, *>}
+   * @private
+   */
+  _memoizedProperties
+
+  /**
+   * Constructor.
+   */
+  constructor() {
+    this._memoizedProperties = new Map()
+  }
+
+  /**
+   * Memoize a property.
+   *
+   * @template T
+   * @param {string} propertyName The property name.
+   * @param {function() : T} valueComputer A function to compute the property's value.
+   * @returns {T} The computed value.
+   * @protected
+   */
+  _memoize(propertyName, valueComputer) {
+    if (!propertyName) {
+      throw new IssueError(
+        generateIssue('internalConsistencyError', { message: 'Invalid property name in Memoizer subclass.' }),
+      )
     }
+    if (this._memoizedProperties.has(propertyName)) {
+      return this._memoizedProperties.get(propertyName)
+    }
+    const value = valueComputer()
+    this._memoizedProperties.set(propertyName, value)
+    return value
   }
 }
-
-/**
- * Memoizer class
- */
-export class Memoizer extends MemoizerMixin(Object) {}
