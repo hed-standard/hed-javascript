6ef9400e82b6a1d5a82b2c13707c6ed34b607b5c
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2024-10-21 19:07:17 -0500

    Separated the tests on the original tokenizer temporarily

--- tests/tockenizerErrorTests.spec.js
+++ tests/tockenizerErrorTests.spec.js
@@ -1,14 +1,9 @@
 import chai from 'chai'
 const assert = chai.assert
 import { beforeAll, describe, afterAll } from '@jest/globals'
-
-import * as hed from '../validator/event'
-import { BidsHedIssue } from '../bids/types/issues'
 import path from 'path'
 import { HedStringTokenizer } from '../parser/tokenizer'
 import { HedStringTokenizerOriginal } from '../parser/tokenizerOriginal'
-//import { HedStringTokenizerNew } from '../parser/tokenizerNew'
-import { generateIssue, IssueError } from '../common/issues/issues'
 import { errorTests } from './tokenizerErrorData'
 const displayLog = process.env.DISPLAY_LOG === 'true'
 const fs = require('fs')
@@ -18,7 +13,7 @@ const skippedErrors = {}
 //const testInfo = loadTestData()
 console.log(errorTests)
 
-describe('HED tokenizer validation using JSON tests', () => {
+describe('Tokenizer validation using JSON tests', () => {
   const badLog = []
   let totalTests = 0
   let wrongErrors = 0
@@ -90,7 +85,79 @@ describe('HED tokenizer validation using JSON tests', () => {
       test.each(tests)('Tokenizer: %s ', (ex) => {
         stringTokenizer(ex.hedCode, ex.code, ex.name, new HedStringTokenizer(ex.string), true, itemLog)
       })
+    }
+  })
+})
 
+describe('Original tokenizer validation using JSON tests', () => {
+  const badLog = []
+  let totalTests = 0
+  let wrongErrors = 0
+  let unexpectedErrors = 0
+
+  beforeAll(async () => {})
+
+  afterAll(() => {
+    const outBad = path.join(__dirname, 'runLogOriginal.txt')
+    const summary = `Total tests:${totalTests} Wrong error codes:${wrongErrors} Unexpected errors:${unexpectedErrors}\n`
+    if (displayLog) {
+      fs.writeFileSync(outBad, summary + badLog.join('\n'), 'utf8')
+    }
+  })
+
+  describe.each(errorTests)('$name : $description', ({ tests }) => {
+    let itemLog
+
+    const assertErrors = function (eHedCode, eCode, expectError, iLog, header, issues) {
+      const log = [header]
+      totalTests += 1
+
+      let errors = []
+      if (issues.length > 0) {
+        errors = issues.map((dict) => dict.hedCode) // list of hedCodes in the issues
+      }
+      const errorString = errors.join(',')
+      if (errors.length > 0) {
+        log.push(`---has errors [${errorString}]`)
+      }
+      const expectedError = eCode
+      const wrongError = `---expected ${eHedCode} but got errors [${errorString}]`
+      const hasErrors = `---expected no errors but got errors [${errorString}]`
+      if (expectError && !errors.includes(eHedCode)) {
+        log.push(wrongError)
+        iLog.push(log.join('\n'))
+        wrongErrors += 1
+        assert.strictEqual(
+          errors.includes(eHedCode),
+          true,
+          `${header}---expected ${eHedCode} and got errors [${errorString}]`,
+        )
+      } else if (!expectError && errors.length > 0) {
+        log.push(hasErrors)
+        iLog.push(log.join('\n'))
+        unexpectedErrors += 1
+        assert(errors.length === 0, `${header}---expected no errors but got errors [${errorString}]`)
+      }
+    }
+
+    const stringTokenizer = function (eHedCode, eCode, eName, tokenizer, expectError, iLog) {
+      const status = expectError ? 'Expect fail' : 'Expect pass'
+      const tokType = tokenizer instanceof HedStringTokenizer ? 'New tokenizer' : 'Original tokenizer'
+      const header = `\n[${eHedCode} ${eName} ${tokType}](${status})\tSTRING: "${tokenizer.hedString}"`
+      const [tagSpecs, groupBounds, tokenizingIssues] = tokenizer.tokenize()
+      const issues = Object.values(tokenizingIssues).flat()
+      assertErrors(eHedCode, eCode, expectError, iLog, header, issues)
+    }
+
+    beforeAll(async () => {
+      itemLog = []
+    })
+
+    afterAll(() => {
+      badLog.push(itemLog.join('\n'))
+    })
+
+    if (tests && tests.length > 0) {
       test.each(tests)('Original tokenizer: %s ', (ex) => {
         stringTokenizer(ex.hedCode, ex.code, ex.name, new HedStringTokenizerOriginal(ex.string), true, itemLog)
       })
--- tests/tokenizerPassingTests.spec.js
+++ tests/tokenizerPassingTests.spec.js
@@ -1,15 +1,9 @@
 import chai from 'chai'
 const assert = chai.assert
 import { beforeAll, describe, afterAll } from '@jest/globals'
-
-import * as hed from '../validator/event'
-import { BidsHedIssue } from '../bids/types/issues'
 import path from 'path'
-//import { HedStringTokenizer } from '../parser/tokenizer'
 import { HedStringTokenizerOriginal } from '../parser/tokenizerOriginal'
-import { TagSpec, GroupSpec, ColumnSpliceSpec, HedStringTokenizer } from '../parser/tokenizer'
-//import { TagSpec, GroupSpec, ColumnSpliceSpec, HedStringTokenizer } from '../parser/tokenizerNew'
-import { generateIssue, IssueError } from '../common/issues/issues'
+import { HedStringTokenizer } from '../parser/tokenizer'
 import { passingTests } from './tokenizerPassingData'
 const fs = require('fs')
 
@@ -17,134 +11,143 @@ const displayLog = process.env.DISPLAY_LOG === 'true'
 
 const skippedErrors = {}
 
-describe('HED tokenizer validation - validData', () => {
-  const badLog = []
-  let totalTests = 0
-  let unexpectedErrors = 0
+describe('HED tokenizer validation', () => {
+  describe('Tokenizer validation - validData', () => {
+    const badLog = []
+    let totalTests = 0
+    let unexpectedErrors = 0
 
-  beforeAll(async () => {})
-
-  afterAll(() => {
-    const outBad = path.join(__dirname, 'runLog.txt')
-    const summary = `Total tests:${totalTests} Unexpected errors:${unexpectedErrors}\n`
-    if (displayLog) {
-      fs.writeFileSync(outBad, summary + badLog.join('\n'), 'utf8')
-    }
-  })
-
-  describe.each(passingTests)('$name : $description', ({ tests }) => {
-    let itemLog
-
-    const assertErrors = function (header, issues, iLog) {
-      iLog.push(`${header}\n`)
-      totalTests += 1
-
-      let errors = []
-      if (issues.length > 0) {
-        errors = issues.map((dict) => dict.hedCode) // list of hedCodes in the issues
-      }
-      const errorString = errors.join(',')
-      if (errors.length > 0) {
-        iLog.push(`---expected no errors but got errors [${errorString}]\n`)
-        unexpectedErrors += 1
-        assert(errors.length === 0, `${header}---expected no errors but got errors [${errorString}]`)
-      }
-    }
-
-    const stringTokenizer = function (eName, tokenizer, tSpecs, gSpec, explanation, iLog) {
-      const status = 'Expect pass'
-      const tokType = tokenizer instanceof HedStringTokenizer ? 'Tokenizer' : 'Original tokenizer'
-      const header = `\n[${tokType}](${status})\tSTRING: "${tokenizer.hedString}"`
-      const [tagSpecs, groupSpec, tokenizingIssues] = tokenizer.tokenize()
-      // Test for no errors
-      const issues = Object.values(tokenizingIssues).flat()
-      assertErrors(header, issues, iLog)
-      assert.sameDeepMembers(tagSpecs, tSpecs, explanation)
-      assert.deepEqual(groupSpec, gSpec, explanation)
-      //assert.sameDeepMembers(groupSpec, gSpec, explanation)
-    }
-
-    beforeAll(async () => {
-      itemLog = []
-    })
+    beforeAll(async () => {})
 
     afterAll(() => {
-      badLog.push(itemLog.join('\n'))
+      const outBad = path.join(__dirname, 'runLog.txt')
+      const summary = `Total tests:${totalTests} Unexpected errors:${unexpectedErrors}\n`
+      if (displayLog) {
+        fs.writeFileSync(outBad, summary + badLog.join('\n'), 'utf8')
+      }
     })
 
-    if (tests && tests.length > 0) {
-      test.each(tests)('Tokenizer: %s ', (ex) => {
-        stringTokenizer(ex.name, new HedStringTokenizer(ex.string), ex.tagSpecs, ex.groupSpec, ex.explanation, itemLog)
+    describe.each(passingTests)('$name : $description', ({ tests }) => {
+      let itemLog
+      const assertErrors = function (header, issues, iLog) {
+        iLog.push(`${header}\n`)
+        totalTests += 1
+
+        let errors = []
+        if (issues.length > 0) {
+          errors = issues.map((dict) => dict.hedCode) // list of hedCodes in the issues
+        }
+        const errorString = errors.join(',')
+        if (errors.length > 0) {
+          iLog.push(`---expected no errors but got errors [${errorString}]\n`)
+          unexpectedErrors += 1
+          assert(errors.length === 0, `${header}---expected no errors but got errors [${errorString}]`)
+        }
+      }
+
+      const stringTokenizer = function (eName, tokenizer, tSpecs, gSpec, explanation, iLog) {
+        const status = 'Expect pass'
+        const tokType = tokenizer instanceof HedStringTokenizer ? 'Tokenizer' : 'Original tokenizer'
+        const header = `\n[${tokType}](${status})\tSTRING: "${tokenizer.hedString}"`
+        const [tagSpecs, groupSpec, tokenizingIssues] = tokenizer.tokenize()
+        // Test for no errors
+        const issues = Object.values(tokenizingIssues).flat()
+        assertErrors(header, issues, iLog)
+        assert.sameDeepMembers(tagSpecs, tSpecs, explanation)
+        assert.deepEqual(groupSpec, gSpec, explanation)
+        //assert.sameDeepMembers(groupSpec, gSpec, explanation)
+      }
+
+      beforeAll(async () => {
+        itemLog = []
       })
 
-      test.each(tests)('Original tokenizer: %s ', (ex) => {
-        stringTokenizer(
-          ex.name,
-          new HedStringTokenizerOriginal(ex.string),
-          ex.tagSpecs,
-          ex.groupSpec,
-          ex.explanation,
-          itemLog,
-        )
+      afterAll(() => {
+        badLog.push(itemLog.join('\n'))
       })
-    }
+
+      if (tests && tests.length > 0) {
+        test.each(tests)('Tokenizer: %s ', (ex) => {
+          stringTokenizer(
+            ex.name,
+            new HedStringTokenizer(ex.string),
+            ex.tagSpecs,
+            ex.groupSpec,
+            ex.explanation,
+            itemLog,
+          )
+        })
+      }
+    })
   })
 
-  describe.each(passingTests)('$name : $description', ({ tests }) => {
-    let itemLog
+  describe('Original tokenizer validation - validData', () => {
+    const badLog = []
+    let totalTests = 0
+    let unexpectedErrors = 0
 
-    const assertErrors = function (header, issues, iLog) {
-      iLog.push(`${header}\n`)
-      totalTests += 1
-
-      let errors = []
-      if (issues.length > 0) {
-        errors = issues.map((dict) => dict.hedCode) // list of hedCodes in the issues
-      }
-      const errorString = errors.join(',')
-      if (errors.length > 0) {
-        iLog.push(`---expected no errors but got errors [${errorString}]\n`)
-        unexpectedErrors += 1
-        assert(errors.length === 0, `${header}---expected no errors but got errors [${errorString}]`)
-      }
-    }
-
-    const stringTokenizer = function (eName, tokenizer, tSpecs, gSpec, explanation, iLog) {
-      const status = 'Expect pass'
-      const tokType = tokenizer instanceof HedStringTokenizer ? 'Tokenizer' : 'Original tokenizer'
-      const header = `\n[${tokType}](${status})\tSTRING: "${tokenizer.hedString}"`
-      const [tagSpecs, groupSpec, tokenizingIssues] = tokenizer.tokenize()
-      // Test for no errors
-      const issues = Object.values(tokenizingIssues).flat()
-      assertErrors(header, issues, iLog)
-      assert.sameDeepMembers(tagSpecs, tSpecs, explanation)
-      assert.deepEqual(groupSpec, gSpec, explanation)
-      //assert.sameDeepMembers(groupSpec, gSpec, explanation)
-    }
-
-    beforeAll(async () => {
-      itemLog = []
-    })
+    beforeAll(async () => {})
 
     afterAll(() => {
-      badLog.push(itemLog.join('\n'))
+      const outBad = path.join(__dirname, 'runLogOriginal.txt')
+      const summary = `Total tests:${totalTests} Unexpected errors:${unexpectedErrors}\n`
+      if (displayLog) {
+        fs.writeFileSync(outBad, summary + badLog.join('\n'), 'utf8')
+      }
     })
 
-    if (tests && tests.length > 0) {
-      test.each(tests)('Tokenizer: %s ', (ex) => {
-        stringTokenizer(ex.name, new HedStringTokenizer(ex.string), ex.tagSpecs, ex.groupSpec, ex.explanation, itemLog)
+    describe.each(passingTests)('$name : $description', ({ tests }) => {
+      let itemLog
+
+      const assertErrors = function (header, issues, iLog) {
+        iLog.push(`${header}\n`)
+        totalTests += 1
+
+        let errors = []
+        if (issues.length > 0) {
+          errors = issues.map((dict) => dict.hedCode) // list of hedCodes in the issues
+        }
+        const errorString = errors.join(',')
+        if (errors.length > 0) {
+          iLog.push(`---expected no errors but got errors [${errorString}]\n`)
+          unexpectedErrors += 1
+          assert(errors.length === 0, `${header}---expected no errors but got errors [${errorString}]`)
+        }
+      }
+
+      const stringTokenizer = function (eName, tokenizer, tSpecs, gSpec, explanation, iLog) {
+        const status = 'Expect pass'
+        const tokType = tokenizer instanceof HedStringTokenizer ? 'Tokenizer' : 'Original tokenizer'
+        const header = `\n[${tokType}](${status})\tSTRING: "${tokenizer.hedString}"`
+        const [tagSpecs, groupSpec, tokenizingIssues] = tokenizer.tokenize()
+        // Test for no errors
+        const issues = Object.values(tokenizingIssues).flat()
+        assertErrors(header, issues, iLog)
+        assert.sameDeepMembers(tagSpecs, tSpecs, explanation)
+        assert.deepEqual(groupSpec, gSpec, explanation)
+        //assert.sameDeepMembers(groupSpec, gSpec, explanation)
+      }
+
+      beforeAll(async () => {
+        itemLog = []
       })
 
-      test.each(tests)('Original tokenizer: %s ', (ex) => {
-        stringTokenizer(
-          ex.name,
-          new HedStringTokenizerOriginal(ex.string),
-          ex.tagSpecs,
-          ex.groupSpec,
-          ex.explanation,
-          itemLog,
-        )
+      afterAll(() => {
+        badLog.push(itemLog.join('\n'))
       })
-    }
+
+      if (tests && tests.length > 0) {
+        test.each(tests)('Original tokenizer: %s ', (ex) => {
+          stringTokenizer(
+            ex.name,
+            new HedStringTokenizerOriginal(ex.string),
+            ex.tagSpecs,
+            ex.groupSpec,
+            ex.explanation,
+            itemLog,
+          )
+        })
+      }
+    })
   })
 })
