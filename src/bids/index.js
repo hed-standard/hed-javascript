c312cc306b9271eb89490973a282f01f77190081
Alexander Jones <happy5214@gmail.com>
2025-02-24 12:18:07 -0600

    Finalize API

--- src/bids/index.js
+++ src/bids/index.js
@@ -2,25 +2,13 @@ import { buildBidsSchemas } from './schema'
 import { BidsJsonFile, BidsSidecar } from './types/json'
 import { BidsTsvFile } from './types/tsv'
 import { BidsHedIssue } from './types/issues'
-import BidsHedSidecarValidator from './validator/sidecarValidator'
-import BidsHedTsvValidator from './validator/tsvValidator'
 
-export {
-  BidsTsvFile,
-  BidsJsonFile,
-  BidsSidecar,
-  BidsHedIssue,
-  BidsHedSidecarValidator,
-  BidsHedTsvValidator,
-  buildBidsSchemas,
-}
+export { BidsTsvFile, BidsJsonFile, BidsSidecar, BidsHedIssue, buildBidsSchemas }
 
 export default {
   BidsTsvFile,
   BidsJsonFile,
   BidsSidecar,
   BidsHedIssue,
-  BidsHedSidecarValidator,
-  BidsHedTsvValidator,
   buildBidsSchemas,
 }
