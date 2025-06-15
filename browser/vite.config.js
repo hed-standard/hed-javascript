599a6c9bdd53efced0ac95cbae923e89284bbd80
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2025-06-15 07:16:13 -0500

    Added lodash to vite configuration

--- browser/vite.config.js
+++ browser/vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig(({ command }) => {
 
     optimizeDeps: {
       // Add this section
-      include: ['xml2js'],
+      include: ['xml2js', 'lodash'], // Added 'lodash'
     },
 
     resolve: {
