945359e4f19dddd3b04840f1075f32fefc950cd6
Alexander Jones <happy5214@gmail.com>
2025-08-07 17:39:55 -0500

    Scrub unused dependencies from configuration scripts

--- jest.config.js
+++ jest.config.js
@@ -5,15 +5,9 @@ module.exports = {
     '\\.xml$': '<rootDir>/xml-transformer.js',
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
-  transformIgnorePatterns: [
-    '/node_modules/(?!unicode-name|date-and-time|semver)',
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    'types/test.ts',
-    '<rootDir>/browser/',
-  ],
+  transformIgnorePatterns: ['/node_modules/(?!unicode-name|semver)'],
+  testPathIgnorePatterns: ['/node_modules/', 'types/test.ts', '<rootDir>/browser/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-};
+}
